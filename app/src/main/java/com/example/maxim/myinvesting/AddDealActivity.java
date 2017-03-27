570d53ea51b9f1a2f0bc80f8dd337b6a2baf3773
taikonavt <taikonavt@gmail.com>
2017-03-27 22:36:06 +0300

    added InvestingProvider insert() stoped on AddDealActivity

---
+++ app/src/main/java/com/example/maxim/myinvesting/AddDealActivity.java
@@ -0,0 +1,10 @@
+package com.example.maxim.myinvesting;
+
+/**
+ * Created by maxim on 27.03.17.
+ */
+
+public class AddDealActivity {
+
+
+}
--- app/src/main/java/com/example/maxim/myinvesting/data/InvestingProvider.java
+++ app/src/main/java/com/example/maxim/myinvesting/data/InvestingProvider.java
@@ -1,20 +1,44 @@
 package com.example.maxim.myinvesting.data;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+
+import static com.example.maxim.myinvesting.data.Contract.DealsEntry.TABLE_NAME;
 
 /**
  * Created by maxim on 26.03.17.
  */
 
 public class InvestingProvider extends ContentProvider{
+
+    public static final int CODE_DEALS = 100;
+    public static final int CODE_DEAL_WITH_DATE = 101;
+
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+    private InvestingDbHelper mOpenHelper;
+
+    public static UriMatcher buildUriMatcher() {
+
+        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+        final String authority = Contract.CONTENT_AUTHORITY;
+
+        matcher.addURI(authority, Contract.PATH_DEALS, CODE_DEALS);
+        matcher.addURI(authority, Contract.PATH_DEALS + "/#", CODE_DEAL_WITH_DATE);
+
+        return  matcher;
+    }
+
     @Override
     public boolean onCreate() {
-        return false;
+        mOpenHelper = new InvestingDbHelper(getContext());
+        return true;
     }
 
     @Nullable
@@ -32,7 +56,29 @@ public class InvestingProvider extends ContentProvider{
     @Nullable
     @Override
     public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
-        return null;
+
+        final SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+
+        Uri returnUri;
+
+        switch (sUriMatcher.match(uri)) {
+
+            case CODE_DEALS:
+
+                long id = db.insert(TABLE_NAME, null, values);
+
+                if (id > 0) {
+                    returnUri = ContentUris.withAppendedId(Contract.DealsEntry.CONTENT_URI, id);
+                } else {
+                    throw new android.database.SQLException("Failed to insert row into " + uri);
+                }
+                break;
+
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+
+        return returnUri;
     }
 
     @Override
