476d66e09ed4041312822039c4e48bdfa3f785c7
taikonavt <taikonavt@gmail.com>
2017-06-14 10:08:35 +0300

    in process of adding InfoFragment

---
+++ app/src/main/java/com/example/maxim/myinvesting/InfoFragment.java
@@ -0,0 +1,91 @@
+package com.example.maxim.myinvesting;
+
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.example.maxim.myinvesting.data.Contract;
+
+/**
+ * Created by maxim on 14.06.17.
+ */
+
+public abstract class InfoFragment extends Fragment
+        implements LoaderManager.LoaderCallbacks<Cursor>{
+
+    private RecyclerView mRecyclerView;
+    protected RecyclerView.Adapter mAdapter;
+
+    private static final int INFO_DEAL_LOADER_ID = 11;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_info, container, false);
+
+        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.rv_info);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        mRecyclerView.setHasFixedSize(true);
+
+        mAdapter = getAdapter();
+
+        mRecyclerView.setAdapter(mAdapter);
+
+        // удаляю потому что относится к MainActivity?
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        getLoaderManager().initLoader(0, null, this);
+
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0,
+                ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder,
+                                  RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+
+                int id = (int) viewHolder.itemView.getTag();
+
+                String stringId = Integer.toString(id);
+                Uri uri = Contract.DealsEntry.CONTENT_URI;
+                uri = uri.buildUpon().appendPath(stringId).build();
+
+                getContentResolver().delete(uri, null, null);
+
+                getLoaderManager().restartLoader(INFO_DEAL_LOADER_ID,
+                        null, InfoFragment.this);
+            }
+        }).attachToRecyclerView(mRecyclerView);
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+
+        getLoaderManager().restartLoader(INFO_DEAL_LOADER_ID, null, this);
+    }
+
+    @Override
+    public abstract Loader<Cursor> onCreateLoader(int id, Bundle args);
+
+    public abstract RecyclerView.Adapter getAdapter();
+}
